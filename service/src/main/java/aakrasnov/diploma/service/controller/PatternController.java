94535460d346b3c7d285df3240430a0867a49c5a
Alexandr <genryxy.alexandr@yandex.ru>
2022-02-13 08:35:17 +0300

    Remove extra code in pattern controller

--- service/src/main/java/aakrasnov/diploma/service/controller/PatternController.java
+++ service/src/main/java/aakrasnov/diploma/service/controller/PatternController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,13 +20,6 @@ public class PatternController {
     @Autowired
     public PatternController(final PatternService ptrnService) {
         this.ptrnService = ptrnService;
-    }
-
-    @GetMapping("pattern/{id}")
-    public ResponseEntity<Pattern> getPattern(@PathVariable("id") String id) {
-        return ptrnService.getById(id)
-            .map(value -> ResponseEntity.status(HttpStatus.FOUND).body(value))
-            .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping("auth/pattern")
